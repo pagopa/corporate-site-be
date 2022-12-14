d8de0ff7cd6d5c5cdcaea79a98c6af899487c11f
justbitdev <justbitdev@justbit.it>
2022-12-14 16:33:13 +0100

    activate debug log

--- config/environments/production.php
+++ config/environments/production.php
@@ -6,7 +6,9 @@
 use Roots\WPConfig\Config;
 
 Config::define('SAVEQUERIES', true);
-Config::define('WP_DEBUG', false);
+Config::define('WP_DEBUG', true);
+Config::define("WP_DEBUG_LOG", true);
+Config::define("GRAPHQL_DEBUG", true);
 Config::define('WP_DEBUG_DISPLAY', false);
 Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
 Config::define('SCRIPT_DEBUG', false);
